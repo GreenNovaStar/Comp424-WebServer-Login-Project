4ec6910ea4a01e765c9af6bb2ccd3ff1d4eb1efc
Alyssa <43042807+AlyssaTW@users.noreply.github.com>
2021-04-28 00:22:58 -0700

    the one that works

--- html/register.php
+++ html/register.php
@@ -1,47 +1,81 @@
 <?php
-session_start();
-$_SESSION['status']="Active";
-$data = parse_ini_file("../config.ini");
-$host=$data[host];
-$user=$data[username];
-$password=$data[password];
-$db=$data[dbname];
-$connection= mysqli_connect($host, $user, $password, $db);
-if($connection === false){
-  die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-//grab information from register page
-$fname = mysqli_real_escape_string($connection, $_POST['fname']);
-$lname = mysqli_real_escape_string($connection, $_POST['lname']);
-$email = mysqli_real_escape_string($connection, $_POST['email']);
-$pwd1 = mysqli_real_escape_string($connection, $_POST['password1']);
-$pwd2 = mysqli_real_escape_string($connection, $_POST['password2']);
+  // session_start();
+  // $_SESSION['status']="Active";
+  $host="localhost";
+  $user="ubuntu";
+  $password="CompClass!424";
+  $db="comp_class";
+  $connection= mysqli_connect($host, $user, $password, $db);
+  //echo "$connection<br>";
+  //echo "0<br>";
+  if($connection -> false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+  }
 
-$querych=mysqli_query($connection,"SELECT email from comp_users where email='$email'");
-$flag=mysqli_num_rows($querych);
-if($flag>=1)
-{
+  $firstName = mysqli_real_escape_string($connection,$_POST['fname']);
+  $lastName = mysqli_real_escape_string($connection,$_POST['lname']);
+  $username = mysqli_real_escape_string($connection,$_POST['usrname']);
+  $email = mysqli_real_escape_string($connection,$_POST['email']);
+  $psw1 = mysqli_real_escape_string($connection,$_POST['psw']);
+  //$sec1 = mysqli_real_escape_string($connection,$_POST['$securityQuestion1']);
+  //$ans1 = mysqli_real_escape_string($connection,$_POST['$question1']);
+  //$sec2 = mysqli_real_escape_string($connection,$_POST['$securityQuestion2']);
+  //$ans2 = mysqli_real_escape_string($connection,$_POST['$question2']);
+  //$sec3 = mysqli_real_escape_string($connection,$_POST['$securityQuestion3']);
+  //$ans3 = mysqli_real_escape_string($connection,$_POST['$question3']);
+  $birthday = mysqli_real_escape_string($connection,$_POST['birthday']);
+
+  $querych=mysqli_query($connection,"SELECT email from comp_users where email='$email'");
+  $flag=mysqli_num_rows($querych);
+  if($flag>=1) {
     echo "User Exists";
     header("Location: login.html");
-}
-else{
-        $sql="INSERT INTO comp_users (fname,lname,email,password) values ('$fname','$lname','$email','$pwd1')";
-        $result=mysqli_query($connection,$sql);
-        if(empty($result))
-        {
-            $create="CREATE TABLE comp_users (
-            fname varchar(255),
-            lname varchar(255),
-            email varchar(255) Primary Key,
-            password varchar(50),
-            dob date,
-            login timestamp
-            )";
-            mysqli_query($connection,$create);
-            mysqli_query($connection,$sql);
-        }
-        header("Location: successful.html");
-}
+  } else{
+    
+    $sql = "INSERT INTO comp_users(
+      fname,
+      lname,
+      uname,
+      email,
+      password,
+      dob
+    )VALUES(
+      '$firstName',
+      '$lastName',
+      '$username',
+      '$email',
+      '$psw1',
+      '$birthday'
+    )";
+	echo "$sql<br>";
+	echo "1<br>";
+	//echo "$connection<br>";
+	//echo "2<br>";
+    $result=mysqli_query($connection,$sql);
+	echo "$result<br>";
+	echo "3<br>";
+    if(empty($result)) {
+      $create="CREATE TABLE comp_users (
+        fname varchar(255),
+        lname varchar(255),
+        uname varchar(50) UNIQUE,
+        email varchar(255) Primary Key,
+        password varchar(50),
+        dob date,
+        login timestamp,
+        numlogin int NOT NULL DEFAULT 0,
+      )";
+		echo "$create";
+		echo "4<br>";
+        $result = mysqli_query($connection,$create);
+		echo "$result";
+		echo "5<br>";
+        $result2 = mysqli_query($connection,$sql);
+		echo "$result2";
+		echo "6<br>";
+    }
+      //header("Location: confirmation.html");
+  }
 
-mysqli_close($connection);
+  mysqli_close($connection);
 ?>
