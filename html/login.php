20cdd33734b26b8a3f1da6ae6981389237453f65
GreenNovaStar <brianyip5@gmail.com>
2021-04-28 00:23:41 -0700

    Update login.php

--- html/login.php
+++ html/login.php
@@ -11,7 +11,7 @@
 
   $connection= mysqli_connect($host, $user, $password, $db);
 
-  if($connection === false){
+  if($connection -> false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
   }
 
